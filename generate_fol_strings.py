425a7697f35be3dbf03a166abb7be10b3fc47759
Ryan Othniel Kearns <ryankearns25@gmail.com>
2024-10-20 12:32:14 +0100

    Improvements to redundancy

--- generate_fol_strings.py
+++ generate_fol_strings.py
@@ -1,6 +1,7 @@
 import argparse
+import re
 
-from itertools import combinations, product
+from itertools import combinations_with_replacement, product
 
 
 def generate_atomic_formulas(variables):
@@ -21,6 +22,39 @@ def combine_binary(formulas1, formulas2):
     return combined
 
 
+def prune_redundant_formulas(formulas):
+    """Prune logically redundant formulas from the set.
+
+    Args:
+        formulas (_type_): _description_
+    """
+    pruned = set()
+    for formula in formulas:
+        # Extract the set of variables in the formula
+        variables = set(
+            [
+                v
+                for v in formula.replace("~", "")
+                .replace("(", "")
+                .replace(")", "")
+                .split(" ")
+                if v.isalpha()
+            ]
+        )
+
+        # Find the character value of the lowest variable in the formula, and subtract
+        # 'A' from it
+        min_variable = ord(min(variables)) - ord("A")
+
+        # Now for each variable in the original formula, match and shift it down by
+        # min_variable
+        for var in variables:
+            formula = formula.replace(var, chr(ord(var) - min_variable))
+        pruned.add(formula)
+
+    return pruned
+
+
 def generate_boolean_formulas(variables, max_depth=2):
     """Generate all Boolean formulas for a set of variables up to a given depth."""
     formulas = generate_atomic_formulas(variables)
@@ -33,13 +67,17 @@ def generate_boolean_formulas(variables, max_depth=2):
     # Iteratively combine formulas up to the given depth
     for depth in range(1, max_depth):
         new_formulas = set()
-        for f1, f2 in combinations(all_formulas, 2):
-            if f1 < f2:
+        for f1, f2 in combinations_with_replacement(all_formulas, 2):
+            f1_alpha = re.sub(r"[^a-zA-Z]", "", f1)
+            f2_alpha = re.sub(r"[^a-zA-Z]", "", f2)
+            if f1_alpha <= f2_alpha:
                 new_formulas.update(combine_binary([f1], [f2]))
         all_formulas.update(new_formulas)
         final_formulas.update(new_formulas)
 
-    return sorted(final_formulas)
+    final_formulas_distinct = prune_redundant_formulas(final_formulas)
+
+    return sorted(final_formulas_distinct)
 
 
 def parse_arguments():
