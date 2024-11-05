489e4fa32ba3cd8465a26d96c15aab0750c7a70f
Ryan Othniel Kearns <ryankearns25@gmail.com>
2024-11-05 14:38:57 +0000

    Add option to add novel disjunct (predicates)

--- etr_case_generator/permute_view.py
+++ etr_case_generator/permute_view.py
@@ -1,9 +1,16 @@
 import random
+import string
 
-# When running with a local build, my linter complains, so tell VSCode to ignore it
-from pyetr.atoms import Atom  # type: ignore
-from pyetr.view import View  # type: ignore
-from pyetr.stateset import State, SetOfStates  # type: ignore
+from pyetr.atoms import Atom
+from pyetr.atoms.predicate import Predicate
+from pyetr.atoms.predicate_atom import PredicateAtom
+from pyetr.stateset import State, SetOfStates
+from pyetr.atoms.terms.function import Function
+from pyetr.atoms.terms.term import FunctionalTerm
+from pyetr.view import View
+
+
+ALL_LETTERS = set(string.ascii_uppercase)
 
 
 def negate_atom(view: View) -> View:
@@ -64,8 +71,36 @@ def negate_atom(view: View) -> View:
     )
 
 
+def add_novel_disjunction(view: View, max_predicate_arity: int = 3) -> View:
+    # Add a single novel state, containing one atom, to the View's stage
+    new_stage = set(view.stage)
+
+    # Use an unreserved letter to create a new predicate
+    vocab = set([atom.predicate.name for atom in view.atoms])
+    available_vars = ALL_LETTERS - vocab
+    new_predicate_name = available_vars.pop()
+
+    # Pick a random arity and populate objects for those places in the new predicate
+    arity = random.randint(1, max_predicate_arity)
+    objects = [available_vars.pop() for _ in range(arity)]
+
+    terms = [FunctionalTerm(f=Function(name=o, arity=0), t=()) for o in objects]
+    new_atom = PredicateAtom(
+        predicate=Predicate(new_predicate_name, arity=arity), terms=tuple(terms)
+    )
+    new_stage.add(State([new_atom]))
+
+    return View(
+        stage=SetOfStates(new_stage),
+        supposition=view.supposition,
+        dependency_relation=view.dependency_relation,
+        issue_structure=view.issue_structure,
+        weights=None,
+    )
+
+
 def permute_view(View) -> View:
-    options = [negate_atom]
+    options = [negate_atom, add_novel_disjunction]
 
     return random.choice(options)(View)
 
