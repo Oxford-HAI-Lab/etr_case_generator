8768f2b1fe273e533a83ad93e249af626419a525
Ryan Othniel Kearns <ryankearns25@gmail.com>
2024-11-23 19:09:06 +0000

    Improve `ReasoningProblem`

--- etr_case_generator/reasoning_problem.py
+++ etr_case_generator/reasoning_problem.py
@@ -2,68 +2,89 @@ from dataclasses import dataclass, field
 from dataclasses_json import config, dataclass_json
 from etr_case_generator.generator import ETRCaseGenerator
 from pyetr import View
-from pyetr.inference import default_inference_procedure
+from pyetr.inference import (
+    default_inference_procedure,
+    default_procedure_does_it_follow,
+)
 from typing import Optional
-
-
-@dataclass
-class Statement:
-    view: View
-    natural_language: str
 
 
 class ReasoningProblem:
     def __init__(self, generator: ETRCaseGenerator):
-        self._premises: list[Statement] = []
-        self.etr_conclusion: Statement = Statement(
-            view=View.get_verum(), natural_language=""
+        self.premises: list[tuple[View, str]] = []
+        self.etr_conclusion: tuple[View, str] = (View.get_verum(), "")
+
+        self.etr_conclusion_is_categorical: bool = False
+        self.etr_conclusion_is_logically_valid: Optional[bool] = (
+            None  # TODO shouldn't be optional
         )
+
         self.generator = generator
         self.obj_map: dict[str, str] = {}
 
-    @property
-    def premises(self):
-        return self._premises
-
-    @premises.setter
-    def premises(self, value: list[Statement]):
-        self._premises = value
-
-        # Run ETR to get the ETR conclusion
-        conclusion = default_inference_procedure([p.view for p in self._premises])
-        self.etr_conclusion = Statement(
-            view=conclusion,
-            natural_language=self.generator.view_to_natural_language(
-                conclusion, obj_map=self.obj_map
-            ),
+        self.query: tuple[View, str] = self.etr_conclusion
+        self.etr_predicts_query_follows: bool = False
+        self.query_is_logically_valid: Optional[bool] = (
+            None  # TODO shouldn't be optional
         )
 
-    # premises: list[tuple[str, str]]
+    def update_premises(self, premises: list[View]):
+        self.premises = []
+        self.obj_map = {}
+        for view in premises:
+            nl, self.obj_map = self.generator.view_to_natural_language(
+                view, self.obj_map
+            )
+            self.premises.append((view, nl))
 
-    # The actual question
-    full_prose: str  # The premises and the question_conclusion
-    question_conclusion: tuple[
-        str, str
-    ]  # Note: this is not necessarily the etr_conclusion
+        # Run ETR to get the ETR conclusion
+        c_etr = default_inference_procedure([p[0] for p in self.premises])
+        c_nl, self.obj_map = self.generator.view_to_natural_language(
+            c_etr, obj_map=self.obj_map
+        )
+        self.etr_conclusion = (
+            c_etr,
+            c_nl,
+        )
 
-    # The ETR conclusion, which is not necessarily what's being asked about
-    etr_conclusion: tuple[str, str]
-    etr_conclusion_is_categorical: bool
+        self.etr_conclusion_is_categorical = (
+            len(c_etr.stage) == 1
+            and not c_etr.stage.is_verum
+            and c_etr.supposition.is_verum
+        )
 
-    # Structured data which makes of the strings. Exclude them because Views aren't serializable.
-    premise_views: list[View] = field(metadata=config(exclude=lambda x: True))
-    etr_conclusion_view: View = field(metadata=config(exclude=lambda x: True))
-    question_conclusion_view: View = field(metadata=config(exclude=lambda x: True))
+        self.etr_predicts_query_follows = default_procedure_does_it_follow(
+            [p[0] for p in self.premises], self.query[0]
+        )
+        # TODO
+        self.etr_conclusion_is_logically_valid = None
 
-    # Is the question conclusion ETR? Is it logically correct?
-    question_conclusion_is_etr_conclusion: Optional[bool] = (
-        None  # The conclusion has one state, no disjunction
-    )
-    classically_valid_conclusion: Optional[bool] = None
+    def append_premise(self, premise: View):
+        self.update_premises([p[0] for p in self.premises] + [premise])
 
-    # More information about the problem
-    vocab_size: Optional[int] = None
-    max_disjuncts: Optional[int] = None
-    num_variables: Optional[int] = None
-    num_disjuncts: Optional[int] = None
-    num_premises: Optional[int] = None
+    def update_query(self, query: View):
+        self.query = (
+            query,
+            self.generator.view_to_natural_language(query, self.obj_map)[0],
+        )
+
+        self.etr_predicts_query_follows = default_procedure_does_it_follow(
+            [p[0] for p in self.premises], self.query[0]
+        )
+        # TODO
+        self.query_is_logically_valid = None
+
+    def full_prose(self) -> str:
+        def punctuate(sentence: str) -> str:
+            return sentence[0].upper() + sentence[1:] + "."
+
+        full_prose = "Consider the following premises:\n\n"
+
+        for i, (_, p_nl) in enumerate(self.premises):
+            full_prose += f"{i + 1}. {punctuate(p_nl)}\n"
+
+        full_prose += "\n"
+        full_prose += f"Does it follow that {punctuate(self.query[1])}?\n\n"
+        full_prose += "Answer using 'YES' or 'NO' ONLY."
+
+        return full_prose
