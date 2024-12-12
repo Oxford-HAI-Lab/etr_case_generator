32e144fac1f28c214e4e943e2025959510ff08fd
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2024-12-12 11:33:55 -0800

    smt problem class

--- etr_case_generator/smt_generator.py
+++ etr_case_generator/smt_generator.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
+from pysmt.shortcuts import Symbol, And, Or, Not, Implies, Iff, is_valid
+from pysmt.fnode import FNode
+from pysmt.typing import BOOL, REAL, PySMTType
 
 
 @dataclass(kw_only=True)
 class SMTProblem:
-    ...
+    views: list[FNode] = None
+
+    # Yes or No format
+    yes_or_no_conclusion_correct: FNode = None
+    yes_or_no_conclusion_incorrect: FNode = None  # Distractor
