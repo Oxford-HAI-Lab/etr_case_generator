c43196276480fc5191c4232b96d71171db6c96e3
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2025-01-10 15:04:58 -0800

    Track whether it matches default_inference_procedure

--- lm_eval/tasks/etr_problems_open_ended/open_ended_scoring.py
+++ lm_eval/tasks/etr_problems_open_ended/open_ended_scoring.py
@@ -8,6 +8,7 @@ from pysmt.fnode import FNode
 from etr_case_generator.generator2.formatting_smt import load_fnode_from_string
 from etr_case_generator.generator2.logic_helper import does_it_follow
 from pyetr.inference import default_procedure_does_it_follow
+from pyetr.inference import default_inference_procedure
 from smt_interface.smt_encoder import view_to_smt
 
 # This is necessary because of the way that lm_eval runs this file
@@ -95,6 +96,8 @@ def score_answer(question, model_answer):
         is_classically_correct: bool = does_it_follow(premises_fnodes, model_view_smt_fnode)
         is_etr_predicted: bool = default_procedure_does_it_follow(premises_view, model_view_etr)
         # TODO Use default_inference_procedure, see if the views match
+        etr_strong_predicted: View = default_inference_procedure(premises_view)
+        is_etr_strong_predicted: bool = etr_strong_predicted == model_view_etr
 
         print(f"ETR predicted: {is_etr_predicted}")
         print(f"Classically correct: {is_classically_correct}")
@@ -102,6 +105,7 @@ def score_answer(question, model_answer):
         return {
             "correct": float(is_classically_correct),
             "etr_predicted": float(is_etr_predicted),
+            "etr_strong_predicted": float(is_etr_strong_predicted),
             "len_response": len(answer_text),
             "parse_error": 0,
         }
