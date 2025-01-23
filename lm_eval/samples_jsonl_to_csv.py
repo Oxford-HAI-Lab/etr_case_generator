554517375fb3e8ffbc0251514433f1ebb735c056
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2025-01-23 13:19:53 -0800

    chore: Add placeholder script for converting JSONL samples to CSV

---
+++ lm_eval/samples_jsonl_to_csv.py
@@ -0,0 +1,15 @@
+
+
+
+
+def main():
+    # TODO argparse
+    # TODO get a file string to match, default of "open_ended"
+
+    # TODO Load all the files in the "lm_eval/tasks/etr_problems/good_results" directory, (including the files one directory deep)
+
+    # TODO Print the number of lines in each file loaded
+    ...
+
+if __name__ == "__main__":
+    main()
