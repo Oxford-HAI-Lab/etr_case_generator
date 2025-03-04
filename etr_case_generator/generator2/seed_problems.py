dd804ac475179a9c7cbc5883035be3eab45b617f
Ryan Othniel Kearns <ryankearns25@gmail.com>
2025-03-04 09:59:48 +0000

    Add reverse premise seed problems

--- etr_case_generator/generator2/seed_problems.py
+++ etr_case_generator/generator2/seed_problems.py
@@ -244,6 +244,109 @@ ILLUSORY_INFERENCE_FROM_DISJUNCTION: list[PartialProblem] = [
     ),
 ]
 
+ILLUSORY_INFERENCE_FROM_DISJUNCTION_REVERSE_PREMISES_TEST: list[PartialProblem] = [
+    PartialProblem(
+        seed_id="target1",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) B(a()), C(b()) D(b()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }"))
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{ B(a()) }")
+        )
+    ),
+    PartialProblem(
+        seed_id="target2",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) B(b()), C(c()) D(d()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }"))
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{ B(b()) }")
+        )
+    ),
+    PartialProblem(
+        seed_id="target3",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()) A(d()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }"))
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{ A(b()) }")
+        )
+    ),
+    PartialProblem(
+        seed_id="target4",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()) A(d()), A(e()) A(f()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }"))
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{ A(b()) }")
+        )
+    ),
+    PartialProblem(
+        seed_id="target5",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()), A(d()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }"))
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{ A(b()) }")
+        )
+    ),
+    PartialProblem(
+        seed_id="control1",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) B(a()), C(b()) D(b()) }")),
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{B(a()),C(b())D(b())}")
+        )
+    ),
+    PartialProblem(
+        seed_id="control2",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) B(b()), C(c()) D(d()) }")),
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{B(b()),C(c())D(d())}")
+        )
+    ),
+    PartialProblem(
+        seed_id="control3",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()) A(d()) }")),
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{A(b()),A(c())A(d())}")
+        )
+    ),
+    PartialProblem(
+        seed_id="control4",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()) A(d()), A(e()) A(f()) }")),
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{A(b()),A(c())A(d()),A(e())A(f())}")
+        )
+    ),
+    PartialProblem(
+        seed_id="control5",
+        premises=[
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) }")),
+            ReifiedView(logical_form_etr_view=View.from_str("{ A(a()) A(b()), A(c()), A(d()) }")),
+        ],
+        etr_what_follows=ReifiedView(
+            logical_form_etr_view=View.from_str("{A(b()),A(c()),A(d())}")
+        )
+    ),
+]
+
 """
 Source: Salvador Mascarenhas, Philipp Koralus. Illusory inferences with quantifiers. Thinking and Reasoning, 2016, 23 (1), pp.33-48.
 
